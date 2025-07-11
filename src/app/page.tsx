7cec2e33cad9864e22ff137caf743572e50141ed
Raul Santana <ti.raul77@gmail.com>
2025-07-11 00:45:38 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="mx-auto my-auto text-amber-300">Hello World</h1>
+      <Button>Bootcamp</Button>
     </div>
   );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/lib/**/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
