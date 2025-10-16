0825612476fbea3b0eee0364e472c2cc4c8f4525
Raul Santana <ti.raul77@gmail.com>
2025-10-15 21:36:46 -0300

    feat: add sign up form

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignUpForm from "./components/sign-up-form";
+
+const AuthenticationPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs defaultValue="login">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger className="cursor-pointer" value="login">
+              Login
+            </TabsTrigger>
+            <TabsTrigger className="cursor-pointer" value="register">
+              Criar conta
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Faça seu login para continuar.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6"></CardContent>
+              <CardFooter>
+                <Button>Entrar</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="register">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
