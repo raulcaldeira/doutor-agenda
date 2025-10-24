0768a256b4c3d56c999646eb1b3df0fc9d2a8ca0
Raul Santana <ti.raul77@gmail.com>
2025-10-23 23:11:08 -0300

    feat: implement session management in authentication and dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,22 @@
-const DashboardPage = () => {
-  return <h1>Dashboard</h1>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+    </div>
+  );
 };
 
 export default DashboardPage;
